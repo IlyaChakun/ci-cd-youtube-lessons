b8444a1c00f6a33b2946d724e38141c18bb9badc
Ilya.Chakun <ilya.chakun@nordstrom.com>
2022-08-22 20:38:25 +0200

    added gitlab ci file added dockerfile

--- src/main/java/test/app/Controller.java
+++ src/main/java/test/app/Controller.java
@@ -1,5 +1,6 @@
 package test.app;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -7,27 +8,14 @@ import org.springframework.web.bind.annotation.*;
 public class Controller {
 
     @GetMapping("/test-get")
-    public String testGet() {
-        return "Get works!";
-    }
-
-    @GetMapping("/test-load-cpu")
-    public String loadCpu() {
-        int numCore = 2;
-        int numThreadsPerCore = 2;
-        double load = 0.8;
-        final long duration = 100000;
-        for (int thread = 0; thread < numCore * numThreadsPerCore; thread++) {
-            new BusyThread("Thread" + thread, load, duration).start();
-        }
-
-        return "done";
+    public ResponseEntity<String> testGet() {
+        return ResponseEntity.ok("Get works!");
     }
 
     @PostMapping("/test-post")
-    public String testPost(@RequestBody TestReq testReq) {
+    public ResponseEntity<String> testPost(@RequestBody TestReq testReq) {
         System.out.println(testReq);
-        return testReq.toString();
+        return ResponseEntity.ok(testReq.toString());
     }
 
 }
